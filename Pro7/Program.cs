194869ae19b46e38953158ffa8e9030d9fca636c
Kala <vdoletskaya@bk.ru>
2022-05-15 22:35:14 +0300

    jj

--- Pro7/Program.cs
+++ Pro7/Program.cs
@@ -47,3 +47,19 @@ while(count < 100000)
   Console.WriteLine("+");
   count ++;
 }
+
+
+
+
+        int n = 1;
+        Console.Write("Введите число ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    
