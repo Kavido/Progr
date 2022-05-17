26754b5b5e6aa37f2a8d002d5a00f213f8d64ae2
Kala <vdoletskaya@bk.ru>
2022-05-17 23:50:07 +0300

    дз

---
+++ Ex15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+   void CheckingTheDayOfTheWeek (int a)
+   { 
+     if (a == 6 || a == 7){
+         Console.WriteLine(" -> да");
+     }
+       else if ( a < 1 || a > 7){
+             Console.WriteLine("Это не день недели >:[");
+       }
+        else Console.WriteLine(" -> нет");
+ }
+ CheckingTheDayOfTheWeek (a);
