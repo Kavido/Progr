8fc68cbcf402ef62e87683c94fd3d57e989246dd
Kala <vdoletskaya@bk.ru>
2022-05-17 20:37:57 +0300

    Попытка 2 отправить в репоз

---
+++ Pro8_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите первое число -> ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число -> ");
+int b = int.Parse(Console.ReadLine());
+
+     Console.Write("a=");
+     Console.Write(a);
+     Console.Write("; b=");
+     Console.Write( b );
+     Console.Write(" -> ");
+
+
+  if (a > b)
+  {
+     Console.Write("max=");
+     Console.WriteLine(a);
+
+
+  } 
+  else
+  {
+     Console.Write("max=");
+     Console.WriteLine(b);
+
+  }
+ 
