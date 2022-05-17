7db028781cc490ce6a42ae444f06cf34c22e6c13
Kala <vdoletskaya@bk.ru>
2022-05-17 21:29:40 +0300

    /

---
+++ Ex8/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 1;
+ Console.WriteLine("Введите число");
+ int b = int.Parse(Console.ReadLine());
+  for( int i = a; i <= b; i++)
+    {
+        if ( i % 2 == 0){
+            Console.Write( i + " ");
+        }
+    }
+  
