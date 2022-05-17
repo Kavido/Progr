1b45f972772a7970f5d6b155038a74db8239b842
Kala <vdoletskaya@bk.ru>
2022-05-17 21:23:00 +0300

    Задача 2

--- Pro10_2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine(" Введите число -> ");
-int a = int.Parse(Console.ReadLine());
- Console.Write(a);
-
- Console.WriteLine( a % 2 == 0 ? " -> Да" : " -> Нет");
--- Pro11_2/obj/Debug/net6.0/Pro11_2.AssemblyInfo.cs
+++ Pro11_2/obj/Debug/net6.0/Pro11_2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Pro10_2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Pro11_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Pro10_2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Pro10_2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Pro11_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Pro11_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
