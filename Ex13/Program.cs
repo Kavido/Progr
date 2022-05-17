5483635743a774cfc538ba206c407e25c7b28238
Kala <vdoletskaya@bk.ru>
2022-05-17 23:36:18 +0300

    задания

---
+++ Ex13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число ->");
+ int a = Convert.ToInt32(Console.ReadLine());
+ string aText = Convert.ToString(a);
+  if ( aText.Length > 2)
+  {
+      Console.WriteLine("Третья цифра -> " + aText[2]);
+  }
+   else 
+   {
+       Console.WriteLine(" -> третьей цифры нет");
+    
+   }
