fbc75c3ad94fd97df260b7c46105958f66cd4e4e
Kala <vdoletskaya@bk.ru>
2022-05-17 20:46:25 +0300

    удалила недопапку Pro9

---
+++ Pro9_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите три числа -> ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write( a );
+Console.Write(", ");
+Console.Write( b );
+Console.Write(", ");
+Console.Write( c );
+Console.Write(" -> ");
+Console.Write(max);
