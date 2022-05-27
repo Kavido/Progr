63094feef8ccd81c7ce21fe3da4071918f2bae44
Kala <vdoletskaya@bk.ru>
2022-05-27 18:06:01 +0300

    дз

--- Ex19/Program.cs
+++ Ex19/Program.cs
@@ -5,7 +5,7 @@ void CheckingNumber(string number){
   if (number[0]==number[4] || number[1]==number[3]){
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+  else Console.WriteLine($"Ваше число: {number} - Нe палиндром.");
 }
 
 if (number!.Length == 5){
