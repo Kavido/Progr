d107d3057858b7ed8b07fba332c36b81002d932e
Kala <vdoletskaya@bk.ru>
2022-05-27 18:27:36 +0300

    дз

---
+++ Ex25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число которое будет возведено в степень: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень в которе будет возведено число: ");
+
+int st = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{a} в степени {st} = " + Math.Pow(a,st));
