59ae25563b7270cb2548977330a259a41ad8c664
Kala <vdoletskaya@bk.ru>
2022-05-27 19:41:37 +0300

    дз

---
+++ Ex27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число: ");
+
+int a = int.Parse(Console.ReadLine());
+ int s = 0;
+    while (a > 0)
+{
+ 
+    s = s + a % 10;
+    a = a /10 ;
+ 
+}
+ Console.WriteLine(s);
