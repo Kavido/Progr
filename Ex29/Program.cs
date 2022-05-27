397e433b52f674396fbb8777c58a09c0aec8eb68
Kala <vdoletskaya@bk.ru>
2022-05-27 21:45:16 +0300

    дз

---
+++ Ex29/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите размер массива:  ");
+int elMass = int.Parse(Console.ReadLine());
+
+int [] array = new int [elMass];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите элементы массива: ");
+array[i] = int.Parse(Console.ReadLine());
+}
+ Console.Write("");
+ 
+     Console.Write("[ ");
+ 
+ for (int i = 0; i < array.Length; i++)
+ {
+     Console.Write($"{array[i]} ");
+    
+ }
+  Console.Write("]");
